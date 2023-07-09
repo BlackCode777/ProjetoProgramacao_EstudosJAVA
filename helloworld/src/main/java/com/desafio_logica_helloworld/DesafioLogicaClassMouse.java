4e43be9b334331807d60cf01b17555210d943852
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-08 22:04:57 -0300

    Estudo de entendimento da condição if/else/for do java

---
+++ helloworld/src/main/java/com/desafio_logica_helloworld/DesafioLogicaClassMouse.java
@@ -0,0 +1,13 @@
+
+package main.java.com.desafio_logica_helloworld;
+
+
+public class DesafioLogicaClassMouse {
+
+    String cor;
+    String modelo;
+    String marca;
+    String tipo;
+    String conexao;
+    
+}
