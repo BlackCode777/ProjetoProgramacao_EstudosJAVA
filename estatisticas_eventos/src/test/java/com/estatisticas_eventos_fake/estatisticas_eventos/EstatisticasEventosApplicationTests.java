6e03ffc0dc368f35c5719fac006bf4c483277418
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-11 23:39:25 -0300

    iniciando o projeto do zero

---
+++ estatisticas_eventos/src/test/java/com/estatisticas_eventos_fake/estatisticas_eventos/EstatisticasEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estatisticas_eventos_fake.estatisticas_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatisticasEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
