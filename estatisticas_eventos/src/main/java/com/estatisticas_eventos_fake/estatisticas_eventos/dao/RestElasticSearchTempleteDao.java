0754801c9867d6b135c0964a31d75d559641a4c1
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-13 19:10:16 -0300

    [Casos_de_Estudo] - Replicando primeiros códigos para teste dos eventos Classe RestElasticSearchTempleteDao.java - criação de métodos entendimento_1.4

--- estatisticas_eventos/src/main/java/com/estatisticas_eventos_fake/estatisticas_eventos/dao/RestElasticSearchTempleteDao.java
+++ estatisticas_eventos/src/main/java/com/estatisticas_eventos_fake/estatisticas_eventos/dao/RestElasticSearchTempleteDao.java
@@ -1,9 +1,42 @@
 package com.estatisticas_eventos_fake.estatisticas_eventos.dao;
 
+import org.json.JSONObject;
 import org.springframework.web.client.RestTemplate;
 
 public class RestElasticSearchTempleteDao {
 
     private static RestTemplate restTemplate = new RestTemplate();
+
+    public static JSONObject consultarOrigem() {
+        return null;
+    }
+
+    public static JSONObject consultaScroll() {
+        return null;
+    }
+
+    public static void inserirRegistro() {
+    }
+
+    public static JSONObject carregarById() {
+        return null;
+    }
+
+    public static boolean registroExiste() {
+        return false;
+    }
+
+    public static void inserirRegistroDireto() {
+    }
+
+    public static void bulk() {
+    }
+
+    public static void deleted() {
+    }
+
+    public static JSONObject consultaRegistro() {
+        return null;
+    }
 
 }
