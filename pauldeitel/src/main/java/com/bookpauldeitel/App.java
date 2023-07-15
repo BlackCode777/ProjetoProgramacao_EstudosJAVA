ee5420031353f1fd321b386784ffd78691dbb89d
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-15 17:38:11 -0300

    ...

--- pauldeitel/src/main/java/com/bookpauldeitel/App.java
+++ pauldeitel/src/main/java/com/bookpauldeitel/App.java
@@ -1,6 +1,8 @@
 package com.bookpauldeitel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,6 +12,26 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
+    }
+
+    // cria uma LinkedList, adiciona elementos e converte em array
+    public static void criaLinkedListAdicionaConvertePArray() {
+        String[] colors = { "black", "blue", "yellow" };
+        LinkedList<String> links = new LinkedList<String>(Arrays.asList(colors));
+
+        links.addLast("red");
+        links.add("pink");
+        links.add(3, "green");// para inserir um elemento em uma posição especifica da lista
+        links.addFirst("Cyan"); // adiciona um elemento no inicio da lista
+        links.add("Magenta");
+
+        System.out.println("LinkedList: ");
+        for (String color : links) {
+            System.out.println(color);
+        }
+    }
+
+    private static List<String> colorRemoved_2() {
         // adiciona elementos no array colors a listar
         String[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
         List<String> list = new ArrayList<String>();
@@ -17,7 +39,10 @@ public class App {
             list.add(color);
             System.out.println(" RESULT <@> " + list);
         }
+        return list;
+    }
 
+    private static void colorRemoved() {
         // Remove elementos da lista
         String[] removeColors = { "RED", "WHITE", "BLUE" };
         List<String> removeList = new ArrayList<String>();
@@ -25,26 +50,24 @@ public class App {
             removeList.add(color);
             System.out.println(removeList);
         }
+    }
 
+    private static void extracted_2(List<String> list) {
         System.out.println("ArrayList: ");
         for (int count = 0; count < list.size(); count++) {
             System.out.printf("%s ", list.get(count));
         }
-        // removeColors(list, removeList);
-
     }
 
-    // private static void removeColors(Collection<String> collection1,
-    // Collection<String> removeList) {
-    // // cria um objeto iterator
-    // Iterator iterator = (Iterator) collection1.iterator();
-    // // itera enquanto a coleção tem itens
-    // while (((java.util.Iterator<String>) iterator).hasNext()) {
-    // // se removeList contém o item atual
-    // if (removeList.contains(iterator.next())) {
-    // ((java.util.Iterator<String>) iterator).remove();
-    // }
-    // }
-    // }
+    private static void getLinkList() {
+        /* 16.6.2 LinkedList */
+        String[] colorLink = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN", "YELLOW", "BLACK", "SILVER", "PURPLE",
+                "ORANGE" };
+        List<String> linkList = new LinkedList<String>();
+        for (String colorList : colorLink) {
+            linkList.add(colorList);
+            System.out.println("Lista Adicionada <> " + linkList);
+        }
+    }
 
 }
