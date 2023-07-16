5cbaab78b422e3e8d9ef3a787f150f6785568055
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-16 00:29:02 -0300

    ...

--- pauldeitel/src/main/java/com/bookpauldeitel/App.java
+++ pauldeitel/src/main/java/com/bookpauldeitel/App.java
@@ -1,7 +1,10 @@
 package com.bookpauldeitel;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,12 +15,108 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
+        // String[] varGlobal = { "Black", "Green", "blue" };
+        // classificandonaOrdemCrescenteComSortTeste(varGlobal);
+
+        // List<Time> list = new ArrayList<Time>();
+        // list.add(new Time(6, 24, 34));
+        // list.add(new Time(18, 14, 58));
+        // System.out.printf("Unsorted array elements:%n%s%n", list);
+        // List<Time> list2 = new ArrayList<Time>();
+        // comparandoDoisParametros(list, list2);
+
+        List<Time> list = new ArrayList<Time>();
+
+        list.add(new Time(6, 24, 34));
+        list.add(new Time(18, 14, 58));
+        list.add(new Time(6, 05, 34));
+        System.out.printf("Unsorted array elements:%n%s%n", list);// .iterator()
+
+        // Collections.sort(list, new TimeComparator());
+
+    }
+
+    private static int comparandoDoisParametros(List<Time> list, List<Time> list2) {
+        int hourDifference = ((Date) list).getHours() - ((Date) list2).getHours();
+        if (hourDifference != 0) {
+            System.out.println("Resultado da comparação - hourDifference : " + hourDifference);
+            return hourDifference;
+        }
+        int minuteDifference = ((Date) list).getMinutes() - ((Date) list2).getMinutes();
+        if (minuteDifference != 0) {
+            System.out.println("Resultado da comparação - minuteDifference : " + minuteDifference);
+            return minuteDifference;
+        }
+        int secondDifference = ((Date) list).getSeconds() - ((Date) list2).getSeconds();
+        System.out.println("Resultado da comparação - secondDifference : " + secondDifference);
+        return hourDifference;
+    }
+
+    // Criando um método de comparação entre dois parametros
+    private int comparandoDoisParametros2(List<Date> list, List<Date> list2) {
+        int hourDifference = ((Date) list).getHours() - ((Date) list2).getHours();
+        if (hourDifference != 0) {
+            return hourDifference;
+        }
+        int minuteDifference = ((Date) list).getMinutes() - ((Date) list2).getMinutes();
+        if (minuteDifference != 0) {
+            return minuteDifference;
+        }
+        int secondDifference = ((Date) list).getSeconds() - ((Date) list2).getSeconds();
+        return secondDifference;
+    }
+
+    private static void classificandonaOrdemCrescenteComSortTeste(String[] varGlobal) {
+        String[] varLocal = varGlobal;
+        List<String> list = Arrays.asList(varLocal);
+        if (list.size() == 0) {
+            System.out.println("Lista vazia");
+        } else {
+            System.out.println("Lista não vazia" + list);
+            // exibir os elementos da lista
+            for (String string : list) {
+                Collections.reverseOrder();
+                System.out.println();
+            }
+        }
+
+    }
+
+    // private static List<String>
+    // classificandonaOrdemCrescenteComSortTeste(List<String> param_1) {
+    // List<String> varLocal = param_1;
+    // // Verificando se a lista esta vazia
+    // if (varLocal.size() == 0) {
+    // System.out.println("Lista vazia");
+    // } else {
+    // // Ordenando a lista
+    // Collections.sort(varLocal);
+    // System.out.println("Lista não vazia");
+
+    // // Invertendo a ordem dos elementos da lista
+    // Collections.reverse(varLocal);
+    // }
+    // return varLocal;
+    // }
+
+    private static void classificandonaOrdemCrescenteComSort(List<String> list2) {
+        List<String> suit = list2;
+        // Verificando se a lista esta vazia
+        if (suit.isEmpty()) {
+            System.out.println("Lista vazia");
+        } else {
+            Collections.sort(suit, Collections.reverseOrder());
+            System.out.println("Lista não vazia");
+        }
+        // Invertendo os elementos da lista
+        // Collections.reverse(suit);
+        System.out.println(suit);
     }
 
     // cria uma LinkedList, adiciona elementos e converte em array
-    public static void criaLinkedListAdicionaConvertePArray() {
+    public static void criaLinkedListAdicionaConvertePArray(LinkedList<String> links) {
         String[] colors = { "black", "blue", "yellow" };
-        LinkedList<String> links = new LinkedList<String>(Arrays.asList(colors));
+        links = new LinkedList<String>(Arrays.asList(colors));
 
         links.addLast("red");
         links.add("pink");
